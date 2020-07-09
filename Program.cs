c94b3756c682fcd17e2f3ab9f5e415cb11c5a5ac
Denis Perepelytsia <d99p01@gmail.com>
2020-07-09 11:35:50 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using Telegram.Bot;
+
+
+
+namespace Status_Bot
+{ 
+    class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("1321277709:AAFyxXEJJJLtQpMorl7WBW7PnItxoIefGXc");
+        
+        public static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("***** Bot Start Work *****\n");
+            Console.WriteLine ($"Bot start working at {DateTime.Now.ToString()}");
+            Console.WriteLine("Press any key to stop bot. \n");
+            
+
+
+            Bot.OnMessage += Bot_OnMessage;
+            Bot.OnMessageEdited += Bot_OnMessage;
+            
+
+           
+           
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+
+        }
+        
+
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            
+            //Input messages
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+                Console.WriteLine($@"Input messages: {e.Message.Text} 
+
+From: {e.Message.Chat.FirstName} Time messages: {DateTime.Now.ToString()}");
+            // Output messages
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                if (e.Message.Text == "/Status")
+                {
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, $"Вітаю вас!{e.Message.Chat.FirstName}");
+
+                    Status_Ping PingOut = new Status_Ping();
+                    Bot.SendTextMessageAsync(e.Message.Chat.Id, $@"Статус наших серверів:
+
+    Репозитарій: {PingOut.output}
+
+    Hosing:  {PingOut.output1}
+
+    Комп: {PingOut.output2} ");
+                }
+
+            }
+            
+           
+        }
+        
+    }    
+}
---
+++ Status_Ping.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using Telegram.Bot;
+
+namespace Status_Bot
+{
+    class Status_Ping
+    {
+        public string output { get; set; }
+        public string output1 { get; set; }
+        public string output2 { get; set; }
+
+        public Status_Ping()
+        {
+            output = Ping();
+            output1 = Ping1();
+            output2 = Ping2();
+        }
+
+        private static string Ping()
+        {
+            string message;
+            Ping ping = new Ping();
+            PingReply pingresult = ping.Send("91.202.144.109");
+            if (pingresult.Status.ToString() == "Success")
+            {
+                message = ($@"Працює ip адреса ({pingresult.Address.ToString()}) Затримка відповіді = { pingresult.RoundtripTime.ToString()} ms");
+            }
+            else
+            {
+                message = ("Не працює! ");
+            }
+            return message;
+        }
+        private static string Ping1()
+        {
+            string message;
+            Ping ping = new Ping();
+            PingReply pingresult = ping.Send("93.190.44.166");
+            if (pingresult.Status.ToString() == "Success")
+            {
+                message = ($@"Працює ip адреса ({pingresult.Address.ToString()}) Затримка відповіді = { pingresult.RoundtripTime.ToString()} ms ");
+            }
+            else
+            {
+                message = ("Не працює! ");
+            }
+            return message;
+        }
+        private static string Ping2()
+        {
+            string message;
+            Ping ping = new Ping();
+            PingReply pingresult = ping.Send("10.11.29.6");
+            if (pingresult.Status.ToString() == "Success")
+            {
+                message = ($@"Працює ip адреса ({pingresult.Address.ToString()}) Затримка відповіді = { pingresult.RoundtripTime.ToString()} ms ");
+            }
+            else
+            {
+                message = ("Не працює! ");
+            }
+            return message;
+        }
+    }
+}
